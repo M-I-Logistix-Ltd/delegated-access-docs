bbfac7f8ff390394b89b7a206f14dbd4cb276ff6
Md Sifatul Islam Rabbi <mdsifatulislam.rabbi@gmail.com>
2022-09-06 22:20:34 +0600

    docs: update ts sample code

--- samples/samples.ts
+++ samples/samples.ts
@@ -14,69 +14,80 @@ const clientId = process.env.CLIENT_ID as string;
 const organizationId = process.env.ORGANIZATION_ID as string;
 const clientSecret = process.env.CLIENT_SECRET as string;
 
-// Helper function to generate the x booker api url
-function getXBookerApiUrl(path: string): string {
-    const base = "https://x-booker.app/delegated/v1";
-    return base + path;
-}
+class DelegatedApp {
+    accessToken: string = "";
+    refreshToken: string = "";
 
-// Helper function to generate the auth api url
-function getXBookerAuthApiUrl(path: string): string {
-    const base = "https://x-booker.app/auth/v1";
-    return base + path;
-}
+    constructor(private readonly clientId: string, private readonly clientSecret: string, private readonly organizationId: string) {}
 
-async function getRefreshToken(): Promise<string> {
-    const url = getXBookerAuthApiUrl("/tokens/refresh");
+    // Helper function to generate the x booker api url
+    getXBookerApiUrl(path: string): string {
+        const base = "https://x-booker.app/delegated/v1";
+        return base + path;
+    }
 
-    const headers = {
-        "organization-id": organizationId,
-        "client-id": clientId,
-        "client-secret": clientSecret,
-    };
-    const response = await axios.get(url, { headers });
-    if (response.data.success) {
-        return response.data.data.refreshToken;
-    } else {
-        throw new Error(response.data.message);
+    // Helper function to generate the auth api url
+    getXBookerAuthApiUrl(path: string): string {
+        const base = "https://x-booker.app/auth/v1";
+        return base + path;
+    }
+
+    async getRefreshToken() {
+        const headers = {
+            "organization-id": this.organizationId,
+            "client-id": this.clientId,
+            "client-secret": this.clientSecret,
+        };
+        const response = await axios.get(this.getXBookerAuthApiUrl("/tokens/refresh"), { headers });
+        if (response.data.success) {
+            this.refreshToken = response.data.data.refreshToken;
+        } else {
+            throw new Error(response.data.message);
+        }
+    }
+
+    async getAccessToken() {
+        if (this.refreshToken === "") {
+            throw new Error("Please call getRefreshToken method first");
+        }
+        const headers = {
+            "organization-id": this.organizationId,
+            "client-id": this.clientId,
+            "Authorization": "Bearer " + this.refreshToken, // Sending the refresh token as Bearer token.
+        };
+        const response = await axios.get(this.getXBookerAuthApiUrl("/tokens/access"), { headers });
+        if (response.data.success) {
+            this.accessToken = response.data.data.accessToken;
+        } else {
+            throw new Error(response.data.message);
+        }
+    }
+
+    async makeRequest<B = any, R = any>(path: string, method: string, body: B): Promise<R> {
+        const headers = {
+            Authorization: "Bearer " + this.accessToken, // Sending the access token as Bearer token.
+        };
+        const payload = body === undefined ? "" : JSON.stringify(body);
+        const response = await axios(this.getXBookerApiUrl(path), {
+            method: method,
+            headers,
+            body: payload,
+        });
+        if (response.data.success) {
+            return response.data.data as R;
+        } else {
+            throw new Error(response.data.message);
+        }
     }
 }
 
-async function getAccessToken(refreshToken: string): Promise<string> {
-    const url = getXBookerAuthApiUrl("/tokens/access");
-
-    const headers = {
-        "organization-id": organizationId,
-        "client-id": clientId,
-        "Authorization": "Bearer " + refreshToken, // Sending the refresh token as Bearer token.
-    };
-    const response = await axios.get(url, { headers });
-    if (response.data.success) {
-        return response.data.data.accessToken;
-    } else {
-        throw new Error(response.data.message);
-    }
-}
-
-async function getOrganizationInfo(accessToken: string): Promise<Organization> {
-    const url = getXBookerApiUrl("/organization");
-    const headers = {
-        Authorization: "Bearer " + accessToken, // Sending the access token as Bearer token.
-    };
-    const response = await axios.get(url, { headers });
-    if (response.data.success) {
-        return response.data.data.organization;
-    } else {
-        throw new Error(response.data.message);
-    }
-}
-
-(async function main(): Promise<void> {
+const delegatedApp = new DelegatedApp(clientId, clientSecret, organizationId);
+(async () => {
     try {
-        const refreshToken = await getRefreshToken();
-        const accessToken = await getAccessToken(refreshToken);
-        const organization = await getOrganizationInfo(accessToken);
-        console.log(organization);
+        await delegatedApp.getRefreshToken();
+        await delegatedApp.getAccessToken();
+        const resp = await delegatedApp.makeRequest<undefined, { data: unknown }>("/organization", "GET", undefined);
+        console.log(resp.data);
     } catch (err) {
         console.error(err);
     }
